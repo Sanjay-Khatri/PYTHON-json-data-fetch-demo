b8093658ed16c46419ff03d62149fc704969373d
Sanjay-Khatri <40001853+Sanjay-Khatri@users.noreply.github.com>
2019-08-23 14:14:49 +0530

    Update assign.py

--- assign.py
+++ assign.py
@@ -1,59 +1,28 @@
-# This Python file uses the following encoding: utf-8
 import json
-
 
 with open("quiz.json") as json_file:
     data = json.load(json_file)
-    print("Press 1 for sports quiz.\nPress 2 for maths quiz.")
-
-
-
-    while True:
-        i = int(input())
-        if i == 1:
-            result=[]
-            sports = data["quiz"]["sport"]
-            print("ANSWER FOLLOWING QUESTIONS BY SELECTING AAPROPRIATE OPTION")
-            for u, v in sports.items():
-                print(v["question"])
-                i = 1
-                for opt in v["options"]:
-                    print(i, ".      ", opt)
-                    i += 1
-                ans = int(input())
-                try:
-                    if str(v["options"][ans - 1]) == v["answer"]:
-                        result.append(1)
-                    else:
-                        result.append(0)
-                except:
+    for u,v in data.items():
+        i=1
+        types = {}
+        for type, question in v.items():
+            print("Press {} for {} quiz.".format(i, type))
+            types[i] = type
+            i +=1
+        select = int(input())
+        result = []
+        for q, ques in v[types.get(select)].items():
+            print(ques["question"])
+            i = 1
+            for opt in ques["options"]:
+                print(i, ".      ", opt)
+                i += 1
+            ans = int(input())
+            try:
+                if str(ques["options"][ans - 1]) == ques["answer"]:
+                    result.append(1)
+                else:
                     result.append(0)
-
-            print("Total Correct Question: ", result.count(1), " out of ", len(sports))
-            break
-
-        elif i == 2:
-            result = []
-            maths = data["quiz"]["maths"]
-            print("ANSWER FOLLOWING QUESTIONS BY SELECTING AAPROPRIATE OPTION")
-            for u, v in maths.items():
-                print(v["question"])
-                i = 1
-                for opt in v["options"]:
-                    print(i, ".      ", opt)
-                    i += 1
-                ans = input()
-                try:
-                    if str(v["options"][ans - 1]) == v["answer"]:
-                        result.append(1)
-                    else:
-                        result.append(0)
-                except:
-                    result.append(0)
-
-            print("Total Correct Question: ", result.count(1), " out of ", len(maths))
-            break
-        else:
-            print("!!! Please select appropriate option from the given options !!!")
-
-
+            except:
+                result.append(0)
+        print("Total Correct Question: ", result.count(1), " out of ", len(result))
